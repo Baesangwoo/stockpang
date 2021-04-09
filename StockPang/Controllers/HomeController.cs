e5ba26e80db6d9961237672499edb53e1ec17406
bsw <baesw13@gmail.com>
2021-04-09 13:33:48 +0900

    ㅎㅎㅎ

--- StockPang/Controllers/HomeController.cs
+++ StockPang/Controllers/HomeController.cs
@@ -46,19 +46,24 @@ namespace StockPang.Controllers
         public ActionResult StockList()
         {
             DataSet data = modelTest.GetAllData();
+            data.Tables[0].Columns.Add("TOTAL_AMT_C");
+            data.Tables[0].Columns.Add("SALES_AMT_C");
+            data.Tables[0].Columns.Add("BIZ_PROFIT_C");
+            data.Tables[0].Columns.Add("NET_PROFIT_C");
 
             foreach(DataRow row in data.Tables[0].Rows)
             {
+
                 string Data01 = row["TOTAL_AMT"].ToString();
                 string Data02 = row["SALES_AMT"].ToString();
                 string Data03 = row["BIZ_PROFIT"].ToString();
                 string Data04 = row["NET_PROFIT"].ToString();
 
                 //문자로 변경한 결과값을 보관하지 못해서 컬럼이 스트링형으로 만들어짐 
-                row["TOTAL_AMT"] = string.Format("{0:# #### #### #### #### ####}", Convert.ToInt32(Data01)).TrimStart().Replace(" ", ",");
-                row["SALES_AMT"] = string.Format("{0:# #### #### #### #### ####}", Convert.ToInt32(Data02)).TrimStart().Replace(" ", ",");
-                row["BIZ_PROFIT"] = string.Format("{0:# #### #### #### #### ####}", Convert.ToInt32(Data03)).TrimStart().Replace(" ", ",");
-                row["NET_PROFIT"] = string.Format("{0:# #### #### #### #### ####}", Convert.ToInt32(Data04)).TrimStart().Replace(" ", ",");
+                row["TOTAL_AMT_C"] = string.Format("{0:# #### #### #### #### ####}", Convert.ToInt32(Data01)).TrimStart().Replace(" ", ",");
+                row["SALES_AMT_C"] = string.Format("{0:# #### #### #### #### ####}", Convert.ToInt32(Data02)).TrimStart().Replace(" ", ",");
+                row["BIZ_PROFIT_C"] = string.Format("{0:# #### #### #### #### ####}", Convert.ToInt32(Data03)).TrimStart().Replace(" ", ",");
+                row["NET_PROFIT_C"] = string.Format("{0:# #### #### #### #### ####}", Convert.ToInt32(Data04)).TrimStart().Replace(" ", ",");
 
             }
 
