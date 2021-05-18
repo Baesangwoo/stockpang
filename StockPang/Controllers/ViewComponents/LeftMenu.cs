0db8cc4391b77652c31955eda4ebc4eed482b3fd
bsw <baesw13@gmail.com>
2021-05-18 16:46:56 +0900

    로고

---
+++ StockPang/Controllers/ViewComponents/LeftMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StockPang.Controllers.ViewComponents
+{
+    public class LeftMenu.cs : Controller
+    {
+        //
+        // GET: /LeftMenu.cs/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
