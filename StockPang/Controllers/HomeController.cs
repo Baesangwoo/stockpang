d68ff2684c520554524d0c7052befc1ef65b9c4a
bsw <baesw13@gmail.com>
2021-06-30 18:24:05 +0900

    화일정리

--- StockPang/Controllers/HomeController.cs
+++ StockPang/Controllers/HomeController.cs
@@ -45,53 +45,7 @@ namespace StockPang.Controllers
 
         // 전체  List 
         [HttpGet]
-        public ActionResult StockList(string Search_name, string Search_class, string Search_psr, string Search_por, string Search_per, string Search_biz_rate, string Search_net_rate)
-        {
-            DataSet data = null;
-            
-            data = modelStockList.GetSearchData(Search_name, Search_class, Search_psr, Search_por, Search_per, Search_biz_rate, Search_net_rate, "All", "0");
-            
-            data.Tables[0].Columns.Add("TOTAL_AMT_C");
-            data.Tables[0].Columns.Add("SALES_AMT_C");
-            data.Tables[0].Columns.Add("BIZ_PROFIT_C");
-            data.Tables[0].Columns.Add("NET_PROFIT_C");
 
-            foreach (DataRow row in data.Tables[0].Rows)
-            {
-
-                string Data01 = row["TOTAL_AMT"].ToString();    //시총 
-                string Data02 = row["SALES_AMT"].ToString();    //매출액 
-                string Data03 = row["BIZ_PROFIT"].ToString();   //영업이익 
-                string Data04 = row["NET_PROFIT"].ToString();   //순이익 
-
-                //문자로 변경한 결과값을 보관하지 못해서 컬럼이 스트링형으로 만들어짐 
-                row["TOTAL_AMT_C"] = string.Format("{0:# #### #### #### #### ####}", Convert.ToInt32(Data01)).TrimStart().Replace(" ", ",");
-                row["SALES_AMT_C"] = string.Format("{0:# #### #### #### #### ####}", Convert.ToInt32(Data02)).TrimStart().Replace(" ", ",");
-
-
-                if (Convert.ToDouble(row["BIZ_PROFIT"].ToString()) >= 0)
-                {
-                    row["BIZ_PROFIT_C"] = string.Format("{0:# #### #### #### #### ####}", Convert.ToInt32(Data03)).TrimStart().Replace(" ", ",");
-                }
-                else
-                {
-                    row["BIZ_PROFIT_C"] = "-" + string.Format("{0:# #### #### #### #### ####}", Convert.ToInt32(Data03) * -1).TrimStart().Replace(" ", ",");
-                }
-
-                if (Convert.ToDouble(row["NET_PROFIT"].ToString()) >= 0)
-                {
-                    row["NET_PROFIT_C"] = string.Format("{0:# #### #### #### #### ####}", Convert.ToInt32(Data04)).TrimStart().Replace(" ", ",");
-                }
-                else
-                {
-                    row["NET_PROFIT_C"] = "-" + string.Format("{0:# #### #### #### #### ####}", Convert.ToInt32(Data04) * -1).TrimStart().Replace(" ", ",");
-                }
-
-            }
-
-            ViewData["VIEWNAME"] = "StockList";
-            return View("StockList", data);
-        }
 
         // User별 List 
         public ActionResult UserStock(string Search_name, string Search_class, string Search_psr, string Search_por, 
@@ -108,7 +62,7 @@ namespace StockPang.Controllers
                 userID = Request.Cookies["USER_INFO"].Values["USER_ID"].ToString();
             }
             
-            data = modelStockList.GetSearchData(Search_name, Search_class, Search_psr, Search_por, Search_per, Search_biz_rate, Search_net_rate, "User", userID);
+            data = modelList1.GetSearchData(Search_name, Search_class, Search_psr, Search_por, Search_per, Search_biz_rate, Search_net_rate, "User", userID);
 
             data.Tables[0].Columns.Add("TOTAL_AMT_C");
             data.Tables[0].Columns.Add("SALES_AMT_C");
@@ -149,7 +103,7 @@ namespace StockPang.Controllers
             }
 
             ViewData["VIEWNAME"] = "UserStock";
-            return View("StockList", data);
+            return View("~/List1/StockList", data);
         }
 
         public ActionResult StockList2( string Search_name, string Search_class, string Search_per, string Search_gap, string Search_sa_rate
--- StockPang/Models/modelStockList.cs
+++ StockPang/Models/modelStockList.cs
@@ -30,62 +30,7 @@ namespace StockPang.Models
 
             return user;
         }
-        public static DataSet GetSearchData(string Search_name, string Search_class, string Search_psr
-                                            , string Search_por, string Search_per, string Search_biz_rate, string Search_net_rate
-                                            , string List_Type, string User_ID
-                                        )
-        {
 
-            string sSql = @"";
-            sSql += "   SELECT  A.STOCK_CODE, A.STOCK_NAME, STOCK_URL, TOTAL_AMT, SALES_AMT, BIZ_PROFIT, NET_PROFIT, PER, EST_PER, BIZ_PER, STOCK_PRICE, NAVER_PRICE, ";
-            sSql += "           CAL_PSR, CAL_POR, CAL_PER, BIZ_RATE, NET_RATE, ";
-            sSql += "           SA_Y3, SA_Y2, SA_Y1, SA_Y0, BP_Y3, BP_Y2, BP_Y1, BP_Y0, NP_Y3, NP_Y2, NP_Y1, NP_Y0, ";
-            sSql += "           SA_RATE, BP_RATE, NP_RATE ,";
-            sSql += "           B.STOCK_REMARK, ISNULL(LEN(B.STOCK_REMARK),0) as REMARK_LEN  ";
-            sSql += "   FROM    STOCK_INFO  A";
-            sSql += "       ,   STOCK_CLASS B";
-            sSql += "   WHERE   A.STOCK_CODE = B.STOCK_CODE ";
-            sSql += "   AND   ( A.STOCK_CODE LIKE '%" + Search_name + "%'" + " OR A.STOCK_NAME LIKE '%" + Search_name + "%')";
-            sSql += "   AND   ( A.STOCK_CLASS1 LIKE '%" + Search_class + "%'" + " OR A.STOCK_CLASS2 LIKE '%" + Search_class + "%')";
-
-            //User별 
-            if (List_Type == "User")
-            {
-                sSql += "   AND   A.STOCK_CODE IN  ( SELECT C.STOCK_CODE FROM USER_STOCK C WHERE C.USER_ID = " + User_ID + " )";
-            }
-
-            if (!string.IsNullOrEmpty(Search_psr) && Search_psr != "")
-            {
-                sSql += "   AND     CAL_PSR   <= " + Search_psr;
-            }
-            if (!string.IsNullOrEmpty(Search_por) && Search_por != "")
-            {
-                sSql += "   AND     CAL_POR   <= " + Search_por;
-            }
-            if (!string.IsNullOrEmpty(Search_per) && Search_per != "")
-            {
-                sSql += "   AND     CAL_PER   <= " + Search_per;
-            }
-            if (!string.IsNullOrEmpty(Search_biz_rate) && Search_biz_rate != "")
-            {
-                sSql += "   AND     BIZ_RATE   >= " + Search_biz_rate;
-            }
-            if (!string.IsNullOrEmpty(Search_net_rate) && Search_net_rate != "")
-            {
-                sSql += "   AND     NET_RATE   >= " + Search_net_rate;
-            }
-            sSql += "   ORDER BY TOTAL_AMT DESC";
-
-
-            DataSet user;
-
-            using (var db = new MSSQLDB())
-            {
-                user = db.Query(sSql);
-            }
-
-            return user;
-        }
 
         public static DataSet GetSearchData2(
                                             string Search_name, string Search_class, string Search_per, string Search_gap
